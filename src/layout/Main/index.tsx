6ef484a55dfa9655c2060355c823889ac2607664
SyedKamranGH <syed.kam.ahmed@gmail.com>
2025-06-05 19:03:03 +0500

    Main component update

--- src/layout/Main/index.tsx
+++ src/layout/Main/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { Box } from "@mui/material";
-import { Outlet } from "react-router-dom";
 import "./styles.scss";
 
-export const Main: React.FC = () => {
+interface MainProps {
+  children: React.ReactNode;
+}
+
+export const Main: React.FC<MainProps> = ({ children }) => {
   return (
-    <Box component="main" className="main">
-      <Outlet />
+    <Box
+      component="main"
+      className="main"
+      sx={{
+        minHeight: "100vh",
+        pt: { xs: "64px", sm: "70px" }, // Account for fixed header
+        pb: 4,
+      }}
+    >
+      {children}
     </Box>
   );
 };
