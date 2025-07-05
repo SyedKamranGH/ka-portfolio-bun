ce7dd782f4e8f2f62aa6d5b89baf3a11fc57cfcb
SyedKamranGH <syed.kam.ahmed@gmail.com>
2025-07-06 03:58:17 +0500

    Improve code readablity

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { Box } from "@mui/material";
 import { Welcome } from "./components/Welcome";
 import { About } from "./components/About";
-// import { Experience } from "./components/Experience";
+import { ExperienceSection } from "./components/Experience";
 // import { Education } from "./components/Education";
 // import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
-import { ExperienceSection } from "./components/Experience";
-// import "./styles.scss";
+
 
 export const Homepage: React.FC = () => {
   return (
